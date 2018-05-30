5d9e403cd42bbdd61890b310d53fd5fe11b5634b
gcky <gcky.cheng@gmail.com>
2018-05-30 18:47:51 +0100

    Add score calculations

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function calcPoints () {
+function calcPoints() {
     console.log("lol")
     let fu = 20;
     if ($('#single-call').prop('checked')) {
@@ -32,11 +32,41 @@ function calcPoints () {
     fu = Math.ceil(fu/10)*10;
     const fan = Number($('#fan').val());
     let basicPoints = 0;
-    console.log(fan)
+    let winnerScore = 0;
+    let jongScore = null;
+    let haanScore = null;
+    let chutchungScore = null;
     if (fan < 5) {
-        console.log(fu)
         basicPoints = fu*Math.pow(2, fan+2)
-        console.log(basicPoints)
-        $('#basic-points').html(basicPoints);
     }
+    if ($('input[name=winner]:checked').val() == 'jong') {
+        if ($('#lon-self').prop('checked')) {
+            haanScore = -2*basicPoints;
+            winnerScore = 6*basicPoints;
+        } else {
+            chutchungScore = -6*basicPoints;
+            winnerScore = 6*basicPoints;
+        }
+    } else {
+        if ($('#lon-self').prop('checked')) {
+            jongScore = -2*basicPoints;
+            haanScore = -basicPoints;
+            winnerScore = 4*basicPoints;
+        } else {
+            chutchungScore = -4*basicPoints;
+            winnerScore = 4*basicPoints;
+        }
+    }
+    displayGameResults(winnerScore, jongScore, haanScore, chutchungScore);
+}
+
+function displayGameResults(winner, jong, haan, chutchong) {
+    $('#winner-score').html('+'+winner);
+    $('#chutchung-score').html(chutchong);
+    $('#jong-score').html(jong);
+    $('#haan-score').html(haan);
+}
+
+function reset() {
+    $('input:checkbox').prop('checked', false);
 }
