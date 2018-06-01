8e7d5f4cbc57acd864f1605924e43f9f1fe3e1a9
gcky <gcky.cheng@gmail.com>
2018-06-01 10:58:16 +0100

    Switch to jquery on function

--- script.js
+++ script.js
@@ -1,63 +1,21 @@
+$(document).on('click', '#calc-btn', calcPoints);
+$(document).on('click', '#reset-btn', resetCalc);
+
 function calcPoints() {
     let fan = Number($('#fan').val());
     let fu = 20;
-    const jongWin = $('#jong-id').val() == $('#winner-id').val();
     if ($('#seven-pairs').prop('checked')) {
         fan -= 1;
         fu = 50;
     } else {
-        if ($('#single-call').prop('checked')) {
-            fu += 2;
-        }
-        if ($('#door-ching').prop('checked') && 
-            !$('#culprit-id').val() == 'self') {
-            fu += 10;
-        }
-        if ($('#culprit-id').val() == 'self' && 
-            !$('#ping-flower').prop('checked') &&
-            !$('#door-ching').prop('checked')) {
-            fu += 2;
-        }
-        if ($('#own-wind-eye').prop('checked')) {
-            fu += 2;
-        }
-        if ($('#board-wind-eye').prop('checked')) {
-            fu += 2;
-        }
-        if ($('#three-dollar-eye').prop('checked')) {
-            fu += 2;
-        }
-        fu += 2*$('#minghak28').val();
-        fu += 4*$('#minghak19').val();
-        fu += 4*$('#amhak28').val();
-        fu += 8*$('#amhak19').val();
-        fu += 8*$('#minggong28').val();
-        fu += 16*$('#minggong19').val();
-        fu += 16*$('#amgong28').val();
-        fu += 32*$('#amgong19').val();
-        fu = Math.ceil(fu/10)*10;
-        if (fan == 1 && fu == 20) {
-            fu = 30;
-        }
+        fu = calcFu(fan);
     }
-    let basicPoints = fu*Math.pow(2, fan+2);
     let winnerScore = 0;
     let jongScore = null;
     let haanScore = null;
     let chutchungScore = null;
-    if (fan == 6 || fan == 7) {
-        basicPoints = 3000;
-    } else if (fan >= 8 && fan <= 10) {
-        basicPoints = 4000;
-    } else if (fan == 11 || fan == 12) {
-        basicPoints = 6000;
-    } else if (fan > 12) {
-        basicPoints = 8000;
-    } else if (fan == 5 || basicPoints > 2000) {
-        basicPoints = 2000;
-    } else if (basicPoints < 2000) {
-        basicPoints = fu*Math.pow(2, fan+2);
-    }
+    let basicPoints = calcBasicPoints(fan, fu);
+    const jongWin = $('#jong-id').val() == $('#winner-id').val();
     if (jongWin) {
         if ($('#culprit-id').val() == 'self') {
             haanScore = -Math.ceil((2*basicPoints)/100)*100;
@@ -79,6 +37,62 @@ function calcPoints() {
     console.log(fu)
     console.log(basicPoints)
     displayGameResults(winnerScore, jongScore, haanScore, chutchungScore);
+}
+
+function calcFu(fan) {
+    let fu = 20;
+    if ($('#single-call').prop('checked')) {
+        fu += 2;
+    }
+    if ($('#door-ching').prop('checked') && 
+        !$('#culprit-id').val() == 'self') {
+        fu += 10;
+    }
+    if ($('#culprit-id').val() == 'self' && 
+        !$('#ping-flower').prop('checked') &&
+        !$('#door-ching').prop('checked')) {
+        fu += 2;
+    }
+    if ($('#own-wind-eye').prop('checked')) {
+        fu += 2;
+    }
+    if ($('#board-wind-eye').prop('checked')) {
+        fu += 2;
+    }
+    if ($('#three-dollar-eye').prop('checked')) {
+        fu += 2;
+    }
+    fu += 2*$('#minghak28').val();
+    fu += 4*$('#minghak19').val();
+    fu += 4*$('#amhak28').val();
+    fu += 8*$('#amhak19').val();
+    fu += 8*$('#minggong28').val();
+    fu += 16*$('#minggong19').val();
+    fu += 16*$('#amgong28').val();
+    fu += 32*$('#amgong19').val();
+    fu = Math.ceil(fu/10)*10;
+    if (fan == 1 && fu == 20) {
+        fu = 30;
+    }
+    return fu;
+}
+
+function calcBasicPoints(fan, fu) {
+    let basicPoints = fu*Math.pow(2, fan+2);
+    if (fan == 6 || fan == 7) {
+        basicPoints = 3000;
+    } else if (fan >= 8 && fan <= 10) {
+        basicPoints = 4000;
+    } else if (fan == 11 || fan == 12) {
+        basicPoints = 6000;
+    } else if (fan > 12) {
+        basicPoints = 8000;
+    } else if (fan == 5 || basicPoints > 2000) {
+        basicPoints = 2000;
+    } else if (basicPoints < 2000) {
+        basicPoints = fu*Math.pow(2, fan+2);
+    }
+    return basicPoints;
 }
 
 function displayGameResults(winnerScore, jongScore, haanScore, culpritScore) {
@@ -108,7 +122,7 @@ function displayGameResults(winnerScore, jongScore, haanScore, culpritScore) {
     }
 }
 
-function reset() {
+function resetCalc() {
     $('input:checkbox').prop('checked', false);
     $('#jong').prop('checked', false);
     $('#haan').prop('checked', true);
@@ -120,42 +134,42 @@ function reset() {
     displayGameResults(null, null, null, null);
 }
 
-function threeDollarClicked() {
+$(document).on('click', '#three-dollar-eye', function () {
     if ($('#three-dollar-eye').prop('checked')) {
         $('#own-wind-eye').prop('checked', false);
         $('#board-wind-eye').prop('checked', false);
     }
-}
+});
 
-function windEyeClicked() {
+$(document).on('click', '#own-wind-eye, #board-wind-eye', function () {
     if ($('#own-wind-eye').prop('checked') ||
         $('#board-wind-eye').prop('checked')) {
         $('#three-dollar-eye').prop('checked', false);
     }
-}
+});
 
-function sevenPairsClicked() {
+$(document).on('click', '#seven-pairs', function () {
     if ($('#seven-pairs').prop('checked')) {
         $('.hakgong tr td select option:first-child').prop('selected', true);
         if ($('#fan').val() < 2) {
             $('#fan').val(2);
         }
     }
-}
+});
 
-function hakgongChanged() {
+$(document).on('change', '.hakgong', function () {
     $('#seven-pairs').prop('checked', false);
-}
+});
 
-function showFanBlocks() {
+$(document).on('click', '.option-link', function () {
     if ($('.options.fan-container').hasClass('show')) {
         $('.options.fan-container').removeClass('show');
     } else {
         $('.options.fan-container').addClass('show');
     }
-}
+});
 
-function winnerChanged() {
+$(document).on('change', '#winner-id', function () {
     const winner = $('#winner-id').val();
     const culprit = $('#culprit-id').val();
     $('#culprit-id option').prop('disabled', false);
@@ -164,7 +178,7 @@ function winnerChanged() {
     }
     $('#culprit-title').prop('disabled', true);
     $('#culprit'+winner).prop('disabled', true);
-}
+});
 
 $(function() {
     $('.player-select').on('change', checkCalcAvailability).change();
@@ -175,16 +189,24 @@ function checkCalcAvailability() {
     $("#calc-btn").attr("disabled", $sels.length > 0);
 }
 
-function resetGameHistory() {
+$(document).on('click', '#reset-hist-btn', function () {
     const startPoints = Number($('#start-points').val());
-    games = [[startPoints, startPoints, startPoints, startPoints]];
+    const games = [[startPoints, startPoints, startPoints, startPoints]];
+    $('#game-table').data('games', games);
     genGameHistoryTable();
-}
+});
 
-var games = [[20000, 20000, 20000, 20000]];
+$(document).on('click', '#add-hist-btn', function () {
+    addHistory(false);
+});
+
+$(document).on('click', '#add-nowin', function () {
+    addHistory(true);
+});
 
 function addHistory(noWin) {
     let newScores = [null, null, null, null];
+    let games = $('#game-table').data('games');
     if (noWin) {
         newScores = games[games.length-1].slice();
     } else {
@@ -193,18 +215,22 @@ function addHistory(noWin) {
         });
     }
     games.push(newScores);
+    $('#game-table').data('games', games);
     genGameHistoryTable();
 }
 
-function removeLastGame() {
+$(document).on('click', '#remove-last', function () {
+    let games = $('#game-table').data('games');
     if (games.length > 1) {
         games.splice(-1,1);
+        $('#game-table').data('games', games);
         genGameHistoryTable();
     }
-}
+});
 
 function genGameHistoryTable() {
     $('.game-table .game-rows').html('');
+    let games = $('#game-table').data('games');
     games.forEach(function (game, idx) {
         let newCells = [$('<td>'), $('<td>'), $('<td>'), $('<td>')];
         if (idx > 0) {
@@ -234,15 +260,20 @@ function genGameHistoryTable() {
     });
 }
 
-function amendScore(player, delta) {
+$(document).on('click', '.amend-btn', function () {
+    const player = $(this).data('player');
+    const delta = $(this).data('amendval');
+    let games = $('#game-table').data('games');
     games[games.length-1][player] += Number(delta);
+    $('#game-table').data('games', games);
     genGameHistoryTable();
-}
+});
 
 window.onbeforeunload = function() {
     return "The game history will be lost, are you sure?";
 };
 
 $(document).ready(function() {
+    $('#game-table').data('games', [[20000, 20000, 20000, 20000]]);
     genGameHistoryTable();
 });
